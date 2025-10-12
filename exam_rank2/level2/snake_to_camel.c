5615756ce287e4563e989f8388bbd80f19531542
Fodé Manca <mancafode11@gmail.com>
2025-10-13 00:02:37 +0100

    finishing snake_to_camel case

--- exam_rank2/level2/snake_to_camel.c
+++ exam_rank2/level2/snake_to_camel.c
@@ -3,18 +3,30 @@
 int	main(int argc, char *argv[])
 {
 	int	i;
+	int upper;
 
 	i = 0;
+	upper = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
 			if (argv[1][i] == '_')
 			{
-				argv[1][i] -= 32;
-				i++;
+				upper = 1;
 			}
-			write (1, &argv[1][i], 1);
+			else
+			{
+				if (upper)
+				{
+					if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+					{
+						argv[1][i] -= 32;
+					}
+					upper = 0;
+				}
+				write (1, &argv[1][i], 1);
+			}
 			i++;
 		}
 	}
