8c6b97f7bde05a2a84f39f410e8583f804b62a20
Fode Manca <fomanca@c1r16s1.42porto.com>
2025-11-17 15:32:00 +0000

    finished

--- exam_rank2/level2/snake_to_camel.c
+++ exam_rank2/level2/snake_to_camel.c
@@ -2,17 +2,18 @@
 
 int	main(int argc, char *argv[])
 {
-	int	i;
-
-	i = 0;
+	int	i = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
 			if (argv[1][i] == '_')
 			{
+				if (argv[1][i + 1] == '\0')
+					break;
 				i++;
-				argv[1][i] -= 32;
+				if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+					argv[1][i] -= 32;
 			}
 			write (1, &argv[1][i], 1);
 			i++;
